fd0d325df0019da99f3c1c359d2602808df37ab1
Artem Lizantan 2023 <plishkan22@gmail.com>
2023-05-25 14:49:45 +0300

    new infoCabinet

--- src/js/modules/user-cabinet/user-info.js
+++ src/js/modules/user-cabinet/user-info.js
@@ -1,253 +1,6 @@
 import JustValidate from "just-validate";
 import axios from "axios";
 
-const contactMarkupFop = (requisites, username, email, phoneNumber) => {
-  return ` <div class="cabinet__contact-wrapper">
-  <button class="cabinet__contact-edit">
-    <img src="./images/cabinet/contact/edit.svg" alt="" />
-  </button>
-  <div class="cabinet__contact-item">
-  <ul class="cabinet__contact-list">
-      <li class="cabinet__contact-list-item">Компания*</li>
-      <li class="cabinet__contact-list-item">${requisites}</li>
-  </ul>
-</div>
-  <div class="cabinet__contact-item">
-      <ul class="cabinet__contact-list">
-          <li class="cabinet__contact-list-item">Контактное лицо*</li>
-          <li class="cabinet__contact-list-item">${username}</li>
-      </ul>
-  </div>
-  <div class="cabinet__contact-item">
-      <ul class="cabinet__contact-list">
-          <li class="cabinet__contact-list-item">Email</li>
-          <li class="cabinet__contact-list-item">${email}</li>
-      </ul>
-  </div>
-  <div class="cabinet__contact-item">
-      <ul class="cabinet__contact-list">
-          <li class="cabinet__contact-list-item">Телефон</li>
-          <li class="cabinet__contact-list-item">${phoneNumber}</li>
-      </ul>
-  </div>
-</div>`;
-};
-
-const contactEditFop = (requisites,username,email, phoneNumber) => {
-  return `
-  <form action="#" class="cabinet__contact-form">
-  <div class="cabinet__contact-wrap">
-      <div class="cabinet__inputs-wrapper">
-        <div class="registration__input-wrap">
-          <input
-            class="registration__input form-company"
-            type="text"
-            placeholder="Компания*"
-            name="company"
-            value="${requisites}"
-          />
-        </div>
-        <div class="registration__input-wrap">
-          <input
-            class="registration__input form-name"
-            type="text"
-            placeholder="Контактное лицо*"
-            name="name"
-            value="${username}"
-          />
-        </div>
-        <div class="registration__input-wrap">
-          <input
-            class="registration__input form-email"
-            type="email"
-            placeholder="Email*"
-            name="email"
-            value="${email}"
-          />
-        </div>
-        <div class="registration__input-wrap">
-          <input
-            class="registration__input form-tel"
-            type="tel"
-            placeholder="Телефон*"
-            name="phoneNumber"
-            value="${phoneNumber}"
-          />
-        </div>
-      </div>
-      <div class="registration__upload">
-        <input
-          type="file"
-          name="file"
-          id="file"
-          class="registration__input-file"
-          name="img"
-        />
-        <label
-          class="registration__upload-label"
-          for="file"
-          class="btn"
-        >
-          <span
-            ><img
-              class="registration__upload-switch"
-              src="./images/registration/overlay.png"
-              alt=""
-          /></span>
-        </label>
-      </div>
-  </div>
-  <div class="cabinet__info-buttons">
-        <button type="submit" class="cabinet__button-submit">
-          Сохранить
-        </button>
-        <button type="button" class="cabinet__info-cancel">
-        Отменить
-      </button>
-      </div>
-</form>
-  `;
-};
-
-//.....................................................
-
-const contactMarkup = (username, email, phoneNumber) => {
-  return ` <div class="cabinet__contact-wrapper">
-  <button class="cabinet__contact-edit">
-    <img src="./images/cabinet/contact/edit.svg" alt="" />
-  </button>
-  <div class="cabinet__contact-item">
-      <ul class="cabinet__contact-list">
-          <li class="cabinet__contact-list-item">Фио</li>
-          <li class="cabinet__contact-list-item">${username}</li>
-      </ul>
-  </div>
-  <div class="cabinet__contact-item">
-      <ul class="cabinet__contact-list">
-          <li class="cabinet__contact-list-item">Email</li>
-          <li class="cabinet__contact-list-item">${email}</li>
-      </ul>
-  </div>
-  <div class="cabinet__contact-item">
-      <ul class="cabinet__contact-list">
-          <li class="cabinet__contact-list-item">Телефон</li>
-          <li class="cabinet__contact-list-item">${phoneNumber}</li>
-      </ul>
-  </div>
-</div>`;
-};
-
-const contactEdit = (username ,email, phoneNumber) => {
-  return `        <form action="#" class="cabinet__contact-form">
-  <div class="cabinet__contact-wrap">
-          <div class="cabinet__inputs-wrapper">
-            <div class="registration__input-wrap">
-              <input
-                class="registration__input form-name"
-                type="text"
-                placeholder="ФИО*"
-                name="name"
-                value="${username}"
-              />
-            </div>
-            <div class="registration__input-wrap">
-              <input
-                class="registration__input form-email"
-                type="email"
-                placeholder="Email*"
-                name="email"
-                value="${email}"
-              />
-            </div>
-            <div class="registration__input-wrap">
-              <input
-                class="registration__input form-tel"
-                type="tel"
-                placeholder="Телефон*"
-                name="phoneNumber"
-                value="${phoneNumber}"
-              />
-            </div>
-          </div>
-          <div class="registration__upload">
-            <input
-              type="file"
-              name="file"
-              id="file"
-              class="registration__input-file"
-              name="img"
-            />
-            <label
-              class="registration__upload-label"
-              for="file"
-              class="btn"
-            >
-              <span
-                ><img
-                  class="registration__upload-switch"
-                  src="./images/registration/overlay.png"
-                  alt=""
-              /></span>
-            </label>
-          </div>
-   </div>
-   <div class="cabinet__info-buttons">
-        <button type="submit" class="cabinet__button-submit">
-          Сохранить
-        </button>
-        <button type="button" class="cabinet__info-cancel">
-        Отменить
-      </button>
-      </div>
-      </form>`;
-};
-
-function contactValidation(fop) {
-  const formCabinet = document.querySelector(".cabinet__contact-form");
-  const formRequisites = document.querySelector(".form-requisites");
-
-  if (formCabinet) {
-    const validate = new JustValidate(formCabinet, {
-      validateBeforeSubmitting: true,
-      lockForm: true,
-      successFieldCssClass: ["valid"],
-    });
-
-    validate.addField(".form-email", [
-      {
-        rule: "required",
-        errorMessage: "Пожалуйста,введите Ваш email",
-      },
-      {
-        rule: "email",
-      },
-    ]);
-    validate.addField(".form-name", [
-      {
-        rule: "required",
-        errorMessage: "Пожалуйста,введите Ваше имя",
-      },
-    ]);
-    validate.addField(".form-tel", [
-      {
-        rule: "required",
-        errorMessage: "Пожалуйста,введите Ваш номер телефона",
-      },
-      {
-        rule: "number",
-      },
-    ]);
-    if (fop) {
-      validate.addField(".form-company", [
-        {
-          rule: "required",
-          errorMessage: "Пожалуйста,введите название компании",
-        },
-      ]);
-    }
-  }
-}
-
 async function contactInfo() {
   let jwt = document.cookie;
   jwt = jwt.split("").splice(4).join("");
@@ -255,41 +8,98 @@ async function contactInfo() {
   const contactWrapper = document.querySelector(".cabinet__contact-info");
 
   if (contactWrapper) {
-    try {
-      const response = await axios.get("http://localhost:1337/api/users/me", {
-        headers: {
-          Authorization: `Bearer ${jwt}`,
-        },
-      });
-      const data = response.data;
-      const email = data.email;
-      const phoneNumber = data.phoneNumber;
-      const username = data.username;
-      const fop = data.fop;
-      const requisites = data.requisites;
+    const cabinetFop = document.getElementById("cabinetFop");
+    const cabinetFopEdit = document.getElementById("cabinetFopEdit");
 
-      if (fop) {
-        contactWrapper.innerHTML = contactMarkupFop(
-          requisites,
-          username,
-          email,
-          phoneNumber
-        );
-        const buttonEdit = document.querySelector(".cabinet__contact-edit");
-        buttonEdit.addEventListener("click", () => {
-          contactWrapper.innerHTML = contactEditFop(requisites,username,email,phoneNumber);
-          contactValidation(fop);
+    const cabinet = document.getElementById("cabinet");
+    const cabinetEdit = document.getElementById("cabinetEdit");
+
+    const cabinetFopButtonEdit = document.getElementById(
+      "cabinetFopButtonEdit"
+    );
+    const cabinetButtonEdit = document.getElementById("cabinetButtonEdit");
+
+    const infoUserFop = cabinetFop.querySelectorAll(".cabinet__fop-info");
+    const userFopInput = cabinetFopEdit.querySelectorAll(".user-fop-input");
+
+    const infoUser = cabinet.querySelectorAll(".cabinet__user-info");
+    const userInput = cabinetEdit.querySelectorAll(".user-input");
+    const cabinetInfoCancel = document.querySelectorAll(
+      ".cabinet__info-cancel"
+    );
+
+    if (contactWrapper) {
+      try {
+        const response = await axios.get("http://localhost:1337/api/users/me", {
+          headers: {
+            Authorization: `Bearer ${jwt}`,
+          },
         });
-      } else {
-        contactWrapper.innerHTML = contactMarkup(username, email, phoneNumber);
-        const buttonEdit = document.querySelector(".cabinet__contact-edit");
-        buttonEdit.addEventListener("click", () => {
-          contactWrapper.innerHTML = contactEdit(username,email,phoneNumber);
-          contactValidation(fop);
-        });
+        const data = response.data;
+
+        const email = data.email;
+        const phoneNumber = data.phoneNumber;
+        const username = data.username;
+        const fop = data.fop;
+        const requisites = data.requisites;
+
+        if (fop) {
+          cabinet.style.display = "none";
+          cabinetEdit.style.display = "none";
+          cabinetFopButtonEdit.addEventListener("click", () => {
+            cabinetFopEdit.classList.add("_active");
+            cabinetFop.classList.add("_hide");
+          });
+
+          infoUserFop[0].innerHTML = requisites;
+          infoUserFop[1].innerHTML = username;
+          infoUserFop[2].innerHTML = email;
+          infoUserFop[3].innerHTML = phoneNumber;
+
+          userFopInput[0].value = requisites;
+          userFopInput[1].value = username;
+          userFopInput[2].value = email;
+          userFopInput[3].value = phoneNumber;
+
+          cabinetInfoCancel.forEach((buttonCancel) => {
+            buttonCancel.addEventListener("click", () => {
+              cabinetFopEdit.classList.remove("_active");
+              cabinetFop.classList.remove("_hide");
+              userFopInput[0].value = requisites;
+              userFopInput[1].value = username;
+              userFopInput[2].value = email;
+              userFopInput[3].value = phoneNumber;
+            });
+          });
+        } else {
+          cabinetFop.style.display = "none";
+          cabinetFopEdit.style.display = "none";
+          cabinetButtonEdit.addEventListener("click", () => {
+            cabinetEdit.classList.add("_active");
+            cabinet.classList.add("_hide");
+          });
+
+          infoUser[0].innerHTML = username;
+          infoUser[1].innerHTML = email;
+          infoUser[2].innerHTML = phoneNumber;
+
+          userInput[0].value = username;
+          userInput[1].value = email;
+          userInput[2].value = phoneNumber;
+
+          cabinetInfoCancel.forEach((buttonCancel) => {
+            buttonCancel.addEventListener("click", () => {
+              cabinetEdit.classList.remove("_active");
+              cabinet.classList.remove("_hide");
+              userInput[0].value = username;
+              userInput[1].value = email;
+              userInput[2].value = phoneNumber;
+            });
+          });
+        }
+      } catch (error) {
+        console.error(error);
       }
-    } catch (error) {
-      console.error(error);
     }
   }
 }
